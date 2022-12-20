27e813cb9f3948968d3d17e7db60760690b5ffe4
Gowtham1303 <110476890+Gowtham1303@users.noreply.github.com>
2022-12-20 16:27:26 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,30 @@
+function preload() {
+}
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    
+
+    tint_color = "";
+}
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+function take_snapshot(){
+    save('student_name.png');
+}
+function filter_tint()
+{
+    tint_color = document.getElementById("color_name").value;
+}
+function take_snapshot(){
+    save('student_name.png');
+}
+circle(30, 30, 20);
+describe('white circle with black outline in mid of gray canvas');
+ellipse(56, 46, 55, 55);
+describe('white ellipse with black outline in middle of a gray canvas');
+rect(30, 20, 55, 55);
+describe('white rect with black outline in mid-right of canvas');
